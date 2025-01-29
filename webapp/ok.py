ecadda78abc431c6aa0681f0c3f86403af79db2c
Sayge1 <101651725+Sayge1@users.noreply.github.com>
2025-01-30 00:37:33 +0300

    Delete webapp directory

--- webapp/ok.py
+++
@@ -1,5 +0,0 @@
-import vt
-client = vt.Client("acff4afdc1c74ab58501bb7eda32e2e66b38e6244391befefa9330c4a3661533")
-url_id = vt.url_id("https://www.roblox.com/home")
-url = client.get_object("/urls/{}", url_id)
-print(url.times_submitted)
